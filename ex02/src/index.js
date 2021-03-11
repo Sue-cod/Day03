da0e698eb3ff6b52a03eb9e3752edd1bdca9971f
Sue-cod <snozic892@gmail.com>
2021-03-11 01:24:04 +0100

    Dodajem komentare

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+   // Only change code below this line
+   // This is a single line JavaScript comment
+
+   /* This is a multi-line comment */
+   // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
